67ac2670094a922ab98b2d67e86e72a1fa90c80c
khushboo.garg <Khushboo Garg>
2022-05-10 11:31:30 +0530

    Cart Functionality

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -10,12 +11,16 @@ export class ProductsComponent implements OnInit {
 
   public productList:any;
 
-  constructor(private api : ApiService) { }
+  constructor(private api : ApiService, private cartService : CartService) { }
 
   ngOnInit(): void {
     this.api.getProduct().subscribe(res=>{
       this.productList=res;
     })
+  }
+
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
   }
 
 }
